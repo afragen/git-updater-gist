31bc0d337cc5a84357be95c2ca8143571167075b
Andy Fragen <andy@thefragens.com>
2021-03-12 14:21:56 -0800

    composer

--- vendor/composer/InstalledVersions.php
+++ vendor/composer/InstalledVersions.php
@@ -25,24 +25,24 @@ class InstalledVersions
 private static $installed = array (
   'root' => 
   array (
-    'pretty_version' => 'dev-develop',
-    'version' => 'dev-develop',
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
     'aliases' => 
     array (
     ),
-    'reference' => 'd937900e6ecf43a086178c1c76dc4d8ce82f2d3b',
+    'reference' => 'b3653d1d46268b13c37ba0e6adb891d096560968',
     'name' => 'afragen/git-updater-gist',
   ),
   'versions' => 
   array (
     'afragen/git-updater-gist' => 
     array (
-      'pretty_version' => 'dev-develop',
-      'version' => 'dev-develop',
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
       'aliases' => 
       array (
       ),
-      'reference' => 'd937900e6ecf43a086178c1c76dc4d8ce82f2d3b',
+      'reference' => 'b3653d1d46268b13c37ba0e6adb891d096560968',
     ),
   ),
 );
