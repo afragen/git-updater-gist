1ac34f66db717801e010fa3bb5e636da20729ea9
Andy Fragen <andy@thefragens.com>
2021-05-21 14:17:06 -0700

    add language pack updating

--- git-updater-gist.php
+++ git-updater-gist.php
@@ -13,13 +13,14 @@
  * Plugin Name:       Git Updater - Gist
  * Plugin URI:        https://github.com/afragen/git-updater-gist
  * Description:       Add GitHub Gist hosted repositories to the Git Updater plugin.
- * Version:           1.0.0.1
+ * Version:           1.0.1
  * Author:            Andy Fragen
  * License:           MIT
  * Network:           true
  * Domain Path:       /languages
  * Text Domain:       git-updater-gist
  * GitHub Plugin URI: https://github.com/afragen/git-updater-gist
+ * GitHub Languages:  https://github.com/afragen/git-updater-gist-translations
  * Primary Branch:    main
  * Requires at least: 5.2
  * Requires PHP:      7.0
